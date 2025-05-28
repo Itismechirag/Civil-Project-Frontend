0b33c7a4cabb8b103628e2bcce40b4f215fa222c
Chirag <chiragupta2005@gmail.com>
2025-05-29 02:05:07 +0530

    host changed

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -17,7 +17,7 @@ import { ExteriorColumnShearForm } from './ShearExteriorColumnForm';
 
 
 
-const host = 'http://127.0.0.1:5000/'
+const host = 'https://civil-project-backend.onrender.com'
 export const Homepage = () => {
 
 
